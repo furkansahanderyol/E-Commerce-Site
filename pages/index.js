f294a2654b25adeec21fa2fef4fe9ebbbe329a01
Furkan <FSDeryol@gmail.com>
2023-04-25 00:22:11 +0300

    filter for price added

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useRef } from "react"
 import Product from "@/components/Product/Product"
 import FilterListElement from "@/components/FilterListElement/FilterListElement"
 import FilterListHeader from "@/components/FilterListHeader/FilterListHeader"
@@ -6,14 +6,31 @@ import styles from "../styles/Home.module.css"
 
 export default function Home({ data }) {
   const [brandsList, setBrandsList] = useState(false)
+  const [priceList, setPriceList] = useState(false)
+  const [minPrice, setMinPrice] = useState(0)
+  const [maxPrice, setMaxPrice] = useState(0)
+  const minPriceRef = useRef()
+  const maxPriceRef = useRef()
 
   const allBrands = data.map((product) => {
     return product.brand
   })
   const brands = [...new Set(allBrands)]
 
-  function handleFilterClick(e) {
+  function handleFilterClick() {
     setBrandsList(!brandsList)
+  }
+
+  function handlePriceListClick() {
+    setPriceList(!priceList)
+  }
+
+  function handleCustomPrice() {
+    const min = minPriceRef.current.value
+    const max = maxPriceRef.current.value
+
+    console.log(min)
+    console.log(max)
   }
 
   return (
@@ -22,7 +39,9 @@ export default function Home({ data }) {
         <h3>Filters</h3>
         <div>
           <div onClick={handleFilterClick}>
-            <FilterListHeader brandsList={brandsList} />
+            <FilterListHeader header={"Featured brands"} list={brandsList} />
+          </div>
+          <div>
             {brandsList ? (
               <ul className={styles.filter_list}>
                 {brands.map((brand, index) => {
@@ -33,6 +52,44 @@ export default function Home({ data }) {
                   )
                 })}
               </ul>
+            ) : null}
+          </div>
+          <div onClick={handlePriceListClick}>
+            <FilterListHeader header={"Price"} list={priceList} />
+          </div>
+          <div className={styles.price_list_wrapper}>
+            {priceList ? (
+              <>
+                <ul
+                  className={`${styles.filter_list} ${styles.filter_list_price}`}
+                >
+                  <li className={styles.price_list_element}>0$ to 25$</li>
+                  <li className={styles.price_list_element}>25$ to 50$</li>
+                  <li className={styles.price_list_element}>50$ to 100$</li>
+                  <li className={styles.price_list_element}>200$ & Above</li>
+                </ul>
+                <div className={styles.price_i}>
+                  <span>
+                    <input
+                      ref={minPriceRef}
+                      className={styles.price_input}
+                      type="number"
+                      placeholder="Min $"
+                    />
+                  </span>
+                  <span>
+                    <input
+                      ref={maxPriceRef}
+                      className={styles.price_input}
+                      type="number"
+                      placeholder="Max $"
+                    />
+                    <button onClick={handleCustomPrice} type="submit">
+                      Go
+                    </button>
+                  </span>
+                </div>
+              </>
             ) : null}
           </div>
 
